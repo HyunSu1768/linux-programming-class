a8a78489e8c4f0776123d718475665fcf2d185ff
HyunSu1768 <azxcv1768@gmail.com>
2024-03-19 12:21:09 +0900

    20240319

---
+++ ex0305.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+	int filedes;
+	off_t newpos;
+	
+	filedes = open("data1.txt", O_RDONLY);
+
+	newpos = lseek(filedes, (off_t)0, SEEK_END);
+
+	printf("filed size : %d\n", newpos);
+}
